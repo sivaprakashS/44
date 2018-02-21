2ae8f3f6e81eab6fe0bc76f5cc1c2cfc1c480f98
sivaprakashS <31438994+sivaprakashS@users.noreply.github.com>
2018-02-21 16:35:35 +0530

    printnum.c

---
+++ printnum.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main()
+{
+	int n,next;
+	printf("\n TO PRINT THE NEXT NUMBER OF THE GIVEN NUMBER");
+	scanf("%d",&n);
+	next=n+1;
+	printf("\n THE NEXT NUMBER OF THE GIVEN NUMBER IS %d",next);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
